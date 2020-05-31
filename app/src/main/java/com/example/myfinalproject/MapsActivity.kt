41926cefbd63cac2fa5fc84c4915a15c63f88d6e
timmyCoder <olatoyedan@gmail.com>
2020-05-31 23:53:00 +0100

    working with places API

--- app/src/main/java/com/example/myfinalproject/MapsActivity.kt
+++ app/src/main/java/com/example/myfinalproject/MapsActivity.kt
@@ -3,7 +3,9 @@ package com.example.myfinalproject
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
@@ -13,12 +15,18 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
 import timber.log.Timber
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +36,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        //initializing places API once activity is created
+        setupPlacesClient()
     }
 
     private fun setupLocationClient() {
@@ -80,6 +90,45 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         getCurrentLocation()
+        map.setOnPoiClickListener {
+            Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun displayPoi(pointOfInterest: PointOfInterest) {
+        val placeId = pointOfInterest.placeId
+
+        val placesFields = listOf(
+            Place.Field.ID,
+            Place.Field.NAME,
+            Place.Field.PHONE_NUMBER,
+            Place.Field.PHOTO_METADATAS,
+            Place.Field.ADDRESS,
+            Place.Field.LAT_LNG
+        )
+
+        val request = FetchPlaceRequest.builder(placeId, placesFields)
+            .build()
+
+        placesClient.fetchPlace(request).addOnSuccessListener { response ->
+            val place = response.place
+            Toast.makeText(
+                this,
+                "${place.name}, " + "${place.phoneNumber}", Toast.LENGTH_LONG
+            ).show()
+        }.addOnFailureListener {
+            exception ->
+            if (exception is ApiException){
+                val statusCode = exception.statusCode
+                Timber.tag(TAG).e("Place not found: "+ exception.message + ", "+ "statusCode: "+ statusCode)
+            }
+        }
+
+    }
+
+    private fun setupPlacesClient() {
+        Places.initialize(applicationContext, "AIzaSyDNmKdyC-UHu-XChS21GQsjxqgyIytXhyY")
+        placesClient = Places.createClient(this)
     }
 
     override fun onRequestPermissionsResult(
